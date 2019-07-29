701c8fe55d1c8cea8336793e3bd7c219daf6a987
Antipascal <pesochin_evgeniy.com>
2019-07-29 19:32:31 +0300

    немного изменений

--- SmartLamp/SmartLamp.ino
+++ SmartLamp/SmartLamp.ino
@@ -1,16 +1,22 @@
 
-#define stripPin 5
-#define stripFrequency 10000
+#define stripPinR 15
+#define stripPinG 16
+#define stripPinB 18
+#define stripFrequency 5000
 #define stripResolution 8
 
 void setup() 
 {
     ledcsetup(0, stripFrequency, stripResolution);
-    ledcAttachPin(stripPin,0);
+    ledcAttachPin(stripPinR,0);
+    ledcAttachPin(stripPinG,1);
+    ledcAttachPin(stripPinB,2);
     // ноль в обоих местах канал шим. Отдельную переменную не выделяю так как шим 
     //будет использоватся только один и поидее менять номер этого канала нам не прийдется
 }
 
 void loop() {
   ledcWrite(0,255);
+  ledcWrite(1,255);
+  ledcWrite(2,255);
 }
