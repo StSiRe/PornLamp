2577fc482883b00a203a7dd5c99cfbf27dac89fc
Hooli Dev <andrei-asdc2@mail.ru>
2020-02-08 21:03:10 +0300

    Update site

--- PlatformIO/Projects/HooliLamp/data/Main/data.js
+++ PlatformIO/Projects/HooliLamp/data/Main/data.js
@@ -1,4 +1,15 @@
+
 function Animations()
 {
-    location.href= "/Animations"
+    var main = document.getElementById("main");
+    var fragment = document.createDocumentFragment()
+    fragment.appendChild(CreateButton());
+    main.appendChild(fragment);
+}
+function CreateButton()
+{
+    var div = document.createElement("div");
+    div.innerHTML = "<div class='Button>" +" dfkdfk"+
+                    "</div>";
+    return div;
 }
