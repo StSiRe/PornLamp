45ff7243e57383c1c14d85662fc2efd79b1e71b3
StSiRe <andrei-asdc2@mail.ru>
2020-02-13 23:34:04 +0300

    Added data.js,animations.js and added new object - progressBar

---
+++ PlatformIO/Projects/HooliLamp/data/Main/Animations.js
@@ -0,0 +1,84 @@
+
+function Animations()   {
+    ClearElements();
+    var main = document.getElementById("main");
+    var fragment = document.createDocumentFragment();
+    fragment.appendChild(CreateForm(
+        Separator(),
+        CreateImage("Animations"),
+        Separator(),
+        CreateText("Анимации:"),
+        CreateToogleButton("PowerMode","PowerModeButton()"),
+        Separator()
+    ));    
+    main.appendChild(fragment);
+}
+
+function PowerModeButton()
+{
+    var button = document.getElementById("PowerMode");
+    alert(button.checked); 
+}
+///Очищает полностью экран от элементов
+function ClearElements()
+{
+    var main = document.getElementById("main");
+    main.remove();
+    var doc = document.createElement("div");
+    doc.setAttribute("id","main");
+    var page = document.getElementsByClassName("page");
+    page[0].appendChild(doc);
+}
+
+function CreateProgressBar(id,onClickHandler)
+{
+    var div = document.createElement("slider");
+    div.classList.add("progress");
+    div.setAttribute("oninput",onClickHandler);
+}
+function CreateText(text)   {
+    var div = document.createElement("text");
+    div.innerText = text;
+    return div
+}
+
+function Separator()   {
+    var div = document.createElement("p");
+    return div
+}
+//Получает свой собственный id, и ссылку на обработкик взаимодействия
+function CreateToogleButton(id,onClickHandler) {
+    var div = document.createElement("label");
+    div.classList.add("switch");
+    div.id = id;
+    
+    var div1 = document.createElement("input");
+    div1.type = "checkbox";
+    div1.setAttribute("onclick",onClickHandler);
+    var div2 = document.createElement("span");
+    div2.classList.add("slider","round");
+    div.appendChild(div1);
+    div.appendChild(div2);
+    return div;
+}
+function CreateForm(...childs) {
+    var div = document.createElement("div");
+    div.className="form";
+    if(childs != null){
+        for (let index = 0; index < childs.length; index++) {
+            div.appendChild(childs[index]);            
+        }
+    }
+    return div;
+}
+function CreateImage(path)  {
+    if(path == null){
+        return null;
+    }        
+    var div = document.createElement("div");
+    div.className="image";
+    var div1 = document.createElement("img");
+    div1.src = "Images/"+ path + ".png";
+    div.appendChild(div1);
+    return div;
+}
--- PlatformIO/Projects/HooliLamp/data/Main/data.js
+++ PlatformIO/Projects/HooliLamp/data/Main/data.js
@@ -1,76 +1,9 @@
-
-function Animations()   {
-    ClearElements();
-    var main = document.getElementById("main");
-    var fragment = document.createDocumentFragment();
-    fragment.appendChild(CreateForm(
-        Separator(),
-        CreateImage("Animations"),
-        Separator(),
-        CreateText("Анимации:"),
-        CreateToogleButton("11","BOC()")  ,
-        Separator()
-    ));    
-    main.appendChild(fragment);
-}
-
-function BOC()
+function LoadData()
 {
-    alert("Fuck you!");
+    let data = fetch("/settings");
+    var json = JSON.parse(data);
 }
-///Очищает полностью экран от элементов
-function ClearElements()
+function SendParameter(param)
 {
-    var main = document.getElementById("main");
-    main.remove();
-    var doc = document.createElement("div");
-    doc.setAttribute("id","main");
-    var page = document.getElementsByClassName("page");
-    page[0].appendChild(doc);
-}
-
-function CreateText(text)   {
-    var div = document.createElement("text");
-    div.innerText = text;
-    return div
-}
-
-function Separator()   {
-    var div = document.createElement("p");
-    return div
-}
-//Получает свой собственный id, и ссылку на обработкик взаимодействия
-function CreateToogleButton(id,onClickHandler) {
-    var div = document.createElement("label");
-    div.classList.add("switch");
-    div.id = id;
-    div.setAttribute("onclick",onClickHandler);
-    var div1 = document.createElement("input");
-    div1.type = "checkbox";
-    var div2 = document.createElement("span");
-    div2.classList.add("slider","round");
-    div.appendChild(div1);
-    div.appendChild(div2);
-    return div;
-}
-function CreateForm(...childs) {
-    var div = document.createElement("div");
-    div.className="form";
-    if(childs != null){
-        for (let index = 0; index < childs.length; index++) {
-            div.appendChild(childs[index]);            
-        }
-    }
-    return div;
-}
-function CreateImage(path)  {
-    if(path == null){
-        return null;
-    }        
-    var div = document.createElement("div");
-    div.className="image";
-    var div1 = document.createElement("img");
-    div1.src = "Images/"+ path + ".png";
-    div.appendChild(div1);
-    return div;
+    
 }
